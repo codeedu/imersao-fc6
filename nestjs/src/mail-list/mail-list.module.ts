ddd2911cb3bfb7947e012e971862b316d9bad781
Luiz Carlos <argentinaluiz@gmail.com>
2022-02-10 08:16:49 -0300

    Config Kafka using env vars

--- nestjs/src/mail-list/mail-list.module.ts
+++ nestjs/src/mail-list/mail-list.module.ts
@@ -11,19 +11,32 @@ import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
     MongooseModule.forFeature([
       { name: MailList.name, schema: MailListSchema },
     ]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'KAFKA_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: 'nest',
-            brokers: ['host.docker.internal:9094'],
+        useFactory: () => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_HOST],
+              ssl: process.env.KAFKA_USE_SSL === 'true',
+              ...(process.env.KAFKA_SASL_USERNAME &&
+                process.env.KAFKA_SASL_USERNAME !== '' &&
+                process.env.KAFKA_SASL_PASSWORD &&
+                process.env.KAFKA_SASL_PASSWORD !== '' && {
+                  sasl: {
+                    mechanism: 'plain',
+                    username: process.env.KAFKA_SASL_USERNAME,
+                    password: process.env.KAFKA_SASL_PASSWORD,
+                  },
+                }),
+            },
           },
           consumer: {
-            groupId: 'nest',
+            groupId: process.env.KAFKA_CONSUMER_GROUP_ID
           },
-        },
+        }),
       },
     ]),
   ],
